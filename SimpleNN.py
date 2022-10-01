5d334c81a93d0f3197573e8262ec9d60fb923b89
Majed Alateeq <87226233+mjdateeq@users.noreply.github.com>
2022-10-01 13:44:27 -0600

    Update and rename nw3.py to SimpleNN.py

--- SimpleNN.py
+++ SimpleNN.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,7 +33,7 @@ def backprop(a2, z0, z1, z2, y):
    return delta2, Delta1, Delta2
 
 
-
+#input and output
 X=np.array ([[1,0,0],
             [1,0,1],
             [1,1,0],
@@ -42,14 +41,8 @@ X=np.array ([[1,0,0],
 y=np.array ([[0],[1],[1],[1]])
 
 # initial weights
-w1 = np.random.randn(3, 3)  # 3 rows - 5 clos
-w2 = np.random.randn(4,1)   # 6 rows - 1 col
-
-print ("Init Weights: ")
-print (w1)
-
-print ("Init Weights: ")
-print (w2)
+w1 = np.random.randn(3, 3) 
+w2 = np.random.randn(4,1)  
 
 
 lr = 0.001
@@ -63,8 +56,6 @@ m=len(X)
 
 for i in range (epochs):
    a1, z1, a2, z2 = forward (X, w1, w2)
-
-
    delta2, Delta1, Delta2 = backprop(a2, X, z1, z2, y)
 
 
@@ -104,4 +95,3 @@ plt.xlabel('Iteration')
 plt.ylabel('Error')
 #plt.savefig('plotxor.jpg')
 plt.show()
-
